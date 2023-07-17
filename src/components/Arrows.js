98623f884ad24e8563a4f83f807ad2a3373c46a4
ManJurkus <129725669+ManJurkus@users.noreply.github.com>
2023-07-17 21:53:44 +0300

    arrow failed

---
+++ src/components/Arrows.js
@@ -0,0 +1,12 @@
+function Arrows() {
+    return (
+                
+                    <div className="col-md-2">
+                       <i class="arrow right"></i>
+                        <i class="arrow left"></i>
+
+                </div>
+            
+        );
+    }
+    export { Arrows };
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import { Arrows } from "./Arrows";
+
+
+function Header() {
+    return (
+
+            <section className="container">
+                <div className="row">
+                
+                    <div className="col-md-12">
+                  <Arrows />
+
+                </div>
+            </div>
+        </section>
+        );
+    }
+    
+    export { Header };
